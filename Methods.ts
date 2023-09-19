527559ebd617151b62151f5f016e1a62cfdd2fab
Jemima Odiri <doveyjem19@gmail.com>
2023-09-19 13:46:42 +0100

    Start4

--- Methods.ts
+++ Methods.ts
@@ -1,61 +1,64 @@
-import http, { IncomingMessage, ServerResponse } from 'http'
-
-const Port:number=6000;
-
+import http, { IncomingMessage, ServerResponse, request } from 'http';
+const Port:number=2100;
 interface iMessage{
          message:string;
-         data:null|[]| {}[]
-         success:boolean;
+         data:null|{}|{}[]
+         success:boolean
 }
 interface iData{
-         name:string;
-         age:number;
+         id:number
+         name:string
+         category:string
+         age:number
 }
-const Person:iData[]=[
+let Student:iData[]=[
          {
-                  name:'John',
-                  age:55
-         }
+                  id:1,
+                  name:'Ayomide',
+                  category:'Back-end',
+                  age:19
+         },
+         {
+                  id:2,
+                  name:'Prince',
+                  category:'Front-end',
+                  age:21
+         },
 ]
 const Server=http.createServer((req:IncomingMessage,res:ServerResponse<IncomingMessage>)=>{
-         res.setHeader('Content-Type', 'application/json');
-
-         const {method,url}=req;
-         let Status:number=404;
-         let Respond:iMessage={
-                  message:'Failed',
-                  data:null,
-                  success:false
-         }
-         const Container:any=[]
-         req.on('data',(chunk:any)=>{
-                  Container.push(chunk)
-         }).on('end',()=>{
-
-
+res.setHeader('Content-Type', 'application/json');
+const {method,url}=req
+let Status:number=404;
+let Response:iMessage={
+message:'Invalid',
+data:null,
+success:false
+}
+const contain:any=[]
+req.on('data',(chunk:any)=>{
+         contain.push(chunk)
+}).on('end',()=>{
          if (url==='/' && method==='GET') {
                   Status=200;
-                  Respond.message='Success getting data'
-                  Respond.success=true;
-                  Respond.data=Person;
-                 res.write(JSON.stringify({Status,Respond}))
-                  res.end();
-
-
-         }
-         if (url==='/' && method==='POST') {
-                  Status=201;
-                  const body=JSON.parse(Container)
-                  Person.push(body)
-                  Respond.message='Success in creating';
-                  Respond.success=true;
-                  Respond.data=Person;
-                  res.write(JSON.stringify({Respond,Status}))
+                  Response.message='Success data of codelab student';
+                  Response.data=Student
+                  res.write(JSON.stringify({Status,Response}))
                   res.end()
          }
-         })
 
+         if (url==='/' && method==='POST') {
+                  Status=201;
+                  const body=JSON.parse(contain)
+                  Student.push(body)
+                  Response.message='Success creating data of another codelab student';
+                  Response.data=Student
+                  Response.success=true
+                  res.write(JSON.stringify({Status,Response}))
+                  res.end()
+         }
 })
+})
+         
 Server.listen(Port,()=>{
          console.log('listening on port',Port)
 })
