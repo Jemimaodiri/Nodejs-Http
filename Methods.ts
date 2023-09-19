989f3ce30cafe8087d1cbcf2f158f62af50bc381
Jemima Odiri <doveyjem19@gmail.com>
2023-09-19 20:35:07 +0100

    stars

--- Methods.ts
+++ Methods.ts
@@ -1,63 +1,69 @@
-import http, { IncomingMessage, ServerResponse } from 'http'
-const port:number=1212;
+import http, { IncomingMessage, ServerResponse } from 'http';
+
+const port:number=4202;
 
 interface iMessage{
          message:string;
-         data:null| {}| {}[];
-         success:boolean;
+         data:null| {}|{}[]
+         success:boolean
 }
 interface iData{
-         name:string;
-         age:number;
-         color:string;
+      name:string;
+      category:string;
+      gender:string;
+
 }
 const Data:iData[]=[
          {
-                  name:'helen',
-                  age:20,
-                  color:'black',
+               name:'Joan',
+               category:'Front-end',
+               gender:'Female',   
          },
          {
-                  name:'helarry',
-                  age:23,
-                  color:'purple',
+               name:'Regina',
+               category:'Back-end',
+               gender:'Female',   
+         },
+         {
+               name:'Jessica',
+               category:'Back-end',
+               gender:'Female',   
          }
 ]
-const Server=http.createServer((req:IncomingMessage,
-         res:ServerResponse<IncomingMessage>)=>{
-
-                  const {url,method}=req
-                 let Status:number=404;
-                  const Response:iMessage={
-                           message:'Failed',
-                           success:false,
-                           data:null,
+const Server=http.createServer((req:IncomingMessage,res:ServerResponse<IncomingMessage>)=>{
+         res.setHeader('Content-Type', 'application/json');
+         const {url,method}=req
+         let Status:number=404;
+         let Response:iMessage={
+                  message: 'Failed',
+                  data: null,
+                  success: false
+         }
+         const Container:any=[];
+         req.on('data',(chunk:any)=>{
+Container.push(chunk);
+         }).on('end',()=>{
+                  
+                  if (url==='/' && method==='GET'){ 
+                           Status=200;
+                           Response.message='Success getting data';
+                           Response.success=true;
+                           Response.data=Data;
+                           res.write(JSON.stringify({Status,Response}))
+                           res.end();
                   }
-                  const Container:any=[]
-                  req.on('data',(chunk:any)=>{
-                  Container.push(chunk)
-                  }).on('end',()=>{
-
-                           if (url==='/' && method==='GET') {
-                                    Status=200;
-                                    Response.message='Succes'
-                                    Response.success=true;
-                                    Response.data=Data
-                                    res.write(JSON.stringify({Status,Response}))
-                                    res.end()
-                           }
-                           if (url==='/' && method==='POST') {
-                                    Status=201;
-                                    const body=JSON.parse(Container)
-                                    Data.push(body)
-                                    Response.message='created'
-                                    Response.success=true;
-                                    Response.data=Data
-                                    res.write(JSON.stringify({Status,Response}))
-                                    res.end()
-                           }
-                  })
+                  if (url==='/' && method==='POST'){ 
+                           Status=201;
+                           const body = JSON.parse(Container)
+                           Data.push(body)
+                           Response.message='Success getting data';
+                           Response.success=true;
+                           Response.data=Data;
+                           res.write(JSON.stringify({Status,Response}))
+                           res.end();
+                  }
+         })
 })
 Server.listen(port,()=>{
-         console.log('Listening on port',port)
+         console.log('Server listening on port',port)
 })
